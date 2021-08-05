559f2a1a73506d94e8d76066bb759ccaf621da7e
Rainer Regan <rainercoc@gmail.com>
2021-08-05 22:20:13 +0700

    Added TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,24 @@ void main() {
         appBar: AppBar(
           title: Text("My App"), // Title didalam AppBar
           centerTitle: true, // Membuat title menjadi center
+          backgroundColor: Colors.red[400], // CTRL+Q untuk dokumentasi
         ),
         body: Center(
-          child: Text("Hello People"),
+          child: Text(
+            "Hello People",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+            ),
+          ),
         ), // Membuat center
         floatingActionButton: FloatingActionButton(
           onPressed: (){},
-          child: Text("click"),
+          child: Text('Click'),
+          backgroundColor: Colors.red[400],
         ),
       ),
   ));
